0f0f1303edb5f96a592526783e6353b01e147848
Matt Vanderpol <matt@vanderpol.net>
2013-03-02 13:25:55 -0800

    Put hostname in prompt to avoid host confusion.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,7 +8,7 @@ local smiley="%(?,%{$fg[green]%}☺%{$reset_color%},%{$fg[red]%}☹%{$reset_colo
 
 # Show the relative path on one line, then the smiley.
 PROMPT='
-%~
+%m:%~
 ${smiley}  %{$reset_color%}'
 
 RPROMPT='%{$fg[white]%} $(~/.rvm/bin/rvm-prompt)$(~/bin/git-cwd-info.rb)%{$reset_color%}'
